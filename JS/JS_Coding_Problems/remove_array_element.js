e1d8e1326a16064772fd99e13a3af47719ed1cad
Shivani Bhalodiya <shivanibhalodiya99@gmail.com>
2023-01-08 23:48:59 +0530

    Project 07 changes

--- JS/JS_Coding_Problems/remove_array_element.js
+++ JS/JS_Coding_Problems/remove_array_element.js
@@ -1,12 +1,24 @@
 function func() {
-  var arr = [1, 2, 4, 8, 4, 6, 0];
+  var arr = [1, 2, 4, 4, 8, 4, 6, 0];
+  let arr1 = [];
+  let arr2 = [];
+  //   for (var i = 0; i < arr.length; i++) {
+  //     if (arr[i] === 4) {
+  //       var spliced = arr.splice(i, 1);
+  //       console.log("spliced", spliced);
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === 4) {
-      var spliced = arr.splice(i, 1);
-      arr.push(spliced[0]);
+  //       arr.push(spliced[0]);
+  //       console.log("arr---", arr);
+  //     }
+  //   }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 4) {
+      arr1.push(arr[i]);
+    } else {
+      arr2.push(arr[i]);
     }
   }
-  console.log(arr);
+  arrRes = [...arr2, ...arr1];
+  console.log(arrRes);
 }
 func();
