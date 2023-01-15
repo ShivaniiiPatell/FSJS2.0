8b359eaf77baeb74f5b32466009d488bbc069059
Shivani Bhalodiya <shivanibhalodiya99@gmail.com>
2023-01-15 22:34:21 +0530

    JS Question 01

---
+++ JS/JS_Assignment/01 Question/variable.js
@@ -0,0 +1,14 @@
+/* 
+01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+*/
+
+let val1 = "Hello There";
+let val2 = true;
+let val3 = undefined;
+let val4 = null;
+
+console.log(`Variable value i ${val1} and type is ${typeof val1}`);
+console.log(`Variable value i ${val2} and type is ${typeof val2}`);
+console.log(`Variable value i ${val3} and type is ${typeof val3}`);
+console.log(`Variable value i ${val4} and type is ${typeof val4}`);
