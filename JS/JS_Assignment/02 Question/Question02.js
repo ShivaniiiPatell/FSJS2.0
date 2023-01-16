435813da543eaa446f5db4a9570c64a51f2d3ec4
Shivani Bhalodiya <shivanibhalodiya99@gmail.com>
2023-01-16 23:40:56 +0530

    JS Question 02

---
+++ JS/JS_Assignment/02 Question/Question02.js
@@ -0,0 +1,19 @@
+/* 
+ 
+Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+*/
+
+function displayInfo() {
+  let firstName = "Shivani";
+  let lastName = "Bhalodiya";
+  let maritalStatus = "Unmarried";
+  let country = "India";
+  let age = 20;
+
+  console.log(
+    `firstName: ${firstName}, lastName: ${lastName}, maritalStatus: ${maritalStatus}, country: ${country}, age: ${age}`
+  );
+}
+
+displayInfo();
