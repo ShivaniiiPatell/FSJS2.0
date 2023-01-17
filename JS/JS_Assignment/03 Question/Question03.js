8e9de57e1618542abfb4755a714aca8e0acc240f
Shivani Bhalodiya <shivanibhalodiya99@gmail.com>
2023-01-17 22:39:13 +0530

    JS Question 03

---
+++ JS/JS_Assignment/03 Question/Question03.js
@@ -0,0 +1,10 @@
+/*
+
+Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+*/
+
+let str = "Shivanibhalodiya";
+
+let str1 = str.toUpperCase();
+console.log(str1);
