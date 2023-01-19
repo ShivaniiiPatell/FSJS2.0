941c2a4748cefe41aca6b4cf4a88eafc97d86333
Shivani Bhalodiya <shivanibhalodiya99@gmail.com>
2023-01-19 23:39:41 +0530

    JS Question 05

---
+++ JS/JS_Assignment/05 Question/Question05.js
@@ -0,0 +1,9 @@
+/*
+    Declare a varibale and assign string value to it and then split it into an array using split() method
+*/
+
+let str = "Hello Shivani How are you?";
+
+strArr = str.split(" ");
+
+console.log(strArr);
