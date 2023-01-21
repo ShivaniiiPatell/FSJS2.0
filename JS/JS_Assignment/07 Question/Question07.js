5b945bc4ad504f68883f0dc80e8e6603e4b8a6ee
Shivani Bhalodiya <shivanibhalodiya99@gmail.com>
2023-01-21 23:35:34 +0530

    JS Question07

---
+++ JS/JS_Assignment/07 Question/Question07.js
@@ -0,0 +1,12 @@
+/*
+
+Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+*/
+
+let str =
+  "Hello pw skills pw skills and pw skills last pw skills hello pw skills";
+
+let laststr = str.lastIndexOf("pw skills", 0);
+
+console.log(laststr);
