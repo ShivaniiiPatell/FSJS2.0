f8ac06b9fb69733a0fbe69cbc3a0dd367bf39643
Shivani Bhalodiya <shivanibhalodiya99@gmail.com>
2023-01-22 23:45:42 +0530

    JS Question08

---
+++ JS/JS_Assignment/08 Question/Question08.js
@@ -0,0 +1,10 @@
+/*
+Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+*/
+
+let str = "    hi how are you    ";
+
+let strnew = str.trim();
+
+console.log(strnew);
