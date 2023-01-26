a7ddba0f5614cea0060aa5321406587459e6c7f6
Shivani Bhalodiya <shivanibhalodiya99@gmail.com>
2023-01-26 23:44:57 +0530

    JS Question 12

---
+++ JS/JS_Assignment/12 Question/Question12.js
@@ -0,0 +1,57 @@
+/*
+    Boolean value is either true or false.
+    - Write three JavaScript statement example which provide truthy value.
+    - Write three JavaScript statement example which provide falsy value.
+*/
+
+/ 3 Truthy values
+
+
+// 1>
+if (true){
+    console.log('1st Truthy value')
+}
+
+// 2>
+if ("abc"){
+    console.log('2nd Truthy value')
+}
+
+// 3>
+if (43){
+    console.log('3rd Truthy value')
+}
+
+
+// 3 Truthy values
+
+
+// 1>
+if (false){
+    consol  e.log('')
+}
+else
+{
+    console.log('1st Falsy value')
+
+}
+
+// 2>
+if (0){
+    console.log('')
+}
+else
+{
+    console.log('2nd Falsy value')
+
+}
+
+// 3>
+if (""){
+    console.log('')
+}
+else
+{
+    console.log('3rd Falsy value')
+
+}
