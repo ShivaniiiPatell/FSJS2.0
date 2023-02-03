a66d2047471d89f358ae0c3369f6ab7968a758eb
Shivani Bhalodiya <shivanibhalodiya99@gmail.com>
2023-02-03 23:49:29 +0530

    JS Question21

---
+++ JS/JS_Assignment/21 Question/Question21.js
@@ -0,0 +1,9 @@
+let countries = ["India", "Canada", "US", "Ethiopia"];
+
+if (countries.includes("Ethiopia")) {
+  console.log("ETHIOPIA");
+} else {
+  countries.push("Ethiopia");
+}
+
+console.log(countries);
